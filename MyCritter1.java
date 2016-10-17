f27cc2b6419859f0250b3ba5557a060c9de57fab
Sean <seanconlon@utexas.edu>
2016-10-17 19:00:55 +0100

    Idea using values to fight

--- MyCritter1.java
+++ MyCritter1.java
@@ -8,10 +8,13 @@ public class MyCritter1 extends Critter.TestCritter {
 	public void doTimeStep() {
 		walk(0);
 	}
-
+	public int getValue(String opponent) {
+		return 3;
+	}
+	
 	@Override
 	public boolean fight(String opponent) {
-		if (getEnergy() > 10) return true;
+		if (getEnergy() > 10 && getValue("THIS CRITTER IDENTITY") > getValue(opponent) ) return true;
 		return false;
 	}
 	

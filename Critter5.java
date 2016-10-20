6c7feeed6435d1052290f4f7a6e15dc3cffa6c6e
Sean <seanconlon@utexas.edu>
2016-10-20 20:10:04 +0100

    JavaDoc included

--- Critter5.java
+++ Critter5.java
@@ -1,5 +1,4 @@
-package assignment4;
-/* CRITTERS Critter5.java
+/* CRITTERS Main.java
  * EE422C Project 4 submission by
  * Replace <...> with your actual data.
  * Mark Carter
@@ -12,36 +11,75 @@ package assignment4;
  * Fall 2016
  */
 
-//Bunny rabbit
-public class Critter5 extends Critter.TestCritter {
+package assignment4;
+/* Critter5 represents a Rabbit
+ * Reproduces as long as it has the energy too
+ * Does not fight
+ * Moves in a square formation
+ */
+
+/*Class name Critter5 */
+public class Critter5 extends Critter {
+	
+	
+	/*Critter5 is represented as a 
+	 * 5 on the print screen */
 	@Override
-	public String toString() { return "5"; }
-	
-	private int lastDir = 0;
-	
+	public String toString() {
+		return "5";
+	}
+
+	/*Declaration of in class variables*/
+	private static final int GENE_TOTAL = 16;
+	private int[] genes5 = new int[8];
+	private int dir;
+
+	/* Generate random direction*/
+	public Critter5() {
+
+		for (int k = 0; k < 8; k += 1) {
+			genes5[k] = GENE_TOTAL / 8;
+		}
+		dir = Critter.getRandomInt(8);
+	}
+
+	/*Critter runs from a fight*/
+	public boolean fight(String opponent) {
+		run(dir);
+		return false;
+
+	}
+
+	/* Critter reproduces during a time - step
+	 * Moves in a circular motion */
 	@Override
 	public void doTimeStep() {
-		// TODO Auto-generated method stub
-		int direction = (lastDir + 1) % 8;
-		walk(direction);
-		lastDir = direction;
-		//Critter will run when possible in a swirling motion
-		
-		//It will also try to reproduce as much as possible
-		if(getEnergy() > Params.min_reproduce_energy) {
-			Critter5 child = new Critter5();
-			reproduce(child, lastDir);
-		}
-	}
-	
-	
-	public boolean fight(String opponent) {
-		return true;
-	 		
+		Critter5 child = new Critter5();
+		reproduce(child, Critter.getRandomInt(8));
+
+		dir += 1;
+		if (dir == 8)
+			dir = 0;
 	}
 
-
+	/* shows the stats of Critter6 */
 	public static void runStats(java.util.List<Critter> MyCritter5s) {
+		int total_straight = 0;
+		int total_left = 0;
+		int total_right = 0;
+		int total_back = 0;
+		for (Object obj : MyCritter5s) {
+			Critter5 c = (Critter5) obj;
+			total_straight += c.genes5[0];
+			total_right += c.genes5[1] + c.genes5[2] + c.genes5[3];
+			total_back += c.genes5[4];
+			total_left += c.genes5[5] + c.genes5[6] + c.genes5[7];
+		}
 		System.out.print("" + MyCritter5s.size() + " total MyCritter5s    ");
+		System.out.print("" + total_straight / (GENE_TOTAL * 0.01 * MyCritter5s.size()) + "% straight   ");
+		System.out.print("" + total_back / (GENE_TOTAL * 0.01 * MyCritter5s.size()) + "% back   ");
+		System.out.print("" + total_right / (GENE_TOTAL * 0.01 * MyCritter5s.size()) + "% right   ");
+		System.out.print("" + total_left / (GENE_TOTAL * 0.01 * MyCritter5s.size()) + "% left   ");
+		System.out.println();
 	}
 }
--- Critter6.java
+++ Critter6.java
@@ -1,5 +1,4 @@
-package assignment4;
-/* CRITTERS Critter6.java
+/* CRITTERS Main.java
  * EE422C Project 4 submission by
  * Replace <...> with your actual data.
  * Mark Carter
@@ -12,19 +11,40 @@ package assignment4;
  * Fall 2016
  */
 
-// MONSTER THAT JUST FIGHTS 
-//Moves in cirles 
-public class Critter6 extends Critter.TestCritter {
+package assignment4;
+/*Critter6 represents a
+ * Monster which only fights
+ * It does not reproduce,  all male species 
+ * Moves in a circular pattern 
+*/
+
+/*Class name Critter6 */
+public class Critter6 extends Critter {
+	
+	/*Critter6 is represented as a 
+	 * 6 on the print screen */
 	@Override
 	public String toString() { return "6"; }
-
+	
+	/*Declaration of in class variables*/
+	
+	private static final int GENE_TOTAL = 16;
+	private int[] genes6 = new int[8];
 	private int dir;
 	private int pos;
 	
+	
+	/* Generate random direction*/
+	
 	public Critter6() {
+		for (int k = 0; k < 8; k += 1) {
+			genes6[k] =GENE_TOTAL / 8;
+		}
 		dir = Critter.getRandomInt(8);
 		pos=0;
 	}
+	
+	/*Critter fights aslong as it is not dead or not*/
 	
 	public boolean fight(String opponent) {
 		if (getEnergy() > 0 ) 
@@ -34,17 +54,39 @@ public class Critter6 extends Critter.TestCritter {
 	 		
 }
 
+	
+	/* Critter runs during a time - step
+	 * Moves in a circular motion */
+	
 	@Override
 	public void doTimeStep() {
-		/* take one step forward */
-		walk(dir);
+		/* take two step forward */
+		run(dir);
 		
 		if(pos==5) dir+=1;
 		if( dir==8) dir=0;
 		pos++;
 	}
 
+	/* shows the stats of Critter6 */
+	
 	public static void runStats(java.util.List<Critter> MyCritter6s) {
+		int total_straight = 0;
+		int total_left = 0;
+		int total_right = 0;
+		int total_back = 0;
+		for (Object obj : MyCritter6s) {
+			Critter6 c = (Critter6) obj;
+			total_straight += c.genes6[0];
+			total_right += c.genes6[1] + c.genes6[2] + c.genes6[3];
+			total_back += c.genes6[4];
+			total_left += c.genes6[5] + c.genes6[6] + c.genes6[7];
+		}
 		System.out.print("" + MyCritter6s.size() + " total MyCritter6s    ");
+		System.out.print("" + total_straight / (GENE_TOTAL * 0.01 * MyCritter6s.size()) + "% straight   ");
+		System.out.print("" + total_back / (GENE_TOTAL * 0.01 * MyCritter6s.size()) + "% back   ");
+		System.out.print("" + total_right / (GENE_TOTAL * 0.01 * MyCritter6s.size()) + "% right   ");
+		System.out.print("" + total_left / (GENE_TOTAL * 0.01 * MyCritter6s.size()) + "% left   ");
+		System.out.println();
 	}
 }
